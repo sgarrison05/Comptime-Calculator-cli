bb4a86766166b96f69bbf2fa81fb5c54cf8d60c9
Shon Garrison <sgarrison05@sbcglobal.net>
2021-09-24 14:14:14 -0500

    Completed basic functionality.  Math is working and the output to both preview and the text files is correct and lining up properly.

--- comptime_cli.py
+++ comptime_cli.py
@@ -1,154 +1,151 @@
 #!/usr/bin/python3
-# home/shon/workspace/comptime.py
+# D:\Programming/github/python/comptime/comptime_cli/comptime_cli.py
 # by Shon Garrison
 # Created on: Aug 1, 2012
-# Updated on: April 20, 2021
+# Updated on: September 24, 2021
 
 import os
+from datetime import date
 
-globalBank = 0.0
-globalPreview =""
+gBank = 0.0
+gPreview = ""
+gDaily_Bank = 0.0
 running = True
+
 
 def quit():
     print("\n")
     print("Application is Now Exiting...")
+    exit()
+
 
 def get_Date():
-    #get the date for the current transaction
-    year, month, day = widget.get_date()
-    trxns_date = (str(month + 1) + "/" + str(day) + "/" + str(year))
+    # get the date for the current transaction
+    today = date.today()
+    answer = input("Do you have a date to enter? (y=enter date, n=today) \n")
 
-def on_clear():
-    print("\n")
-    print("Now Clearing Form...")
+    if answer == "y":
+        m = int(input("Enter Month \n"))
+        d = int(input("Enter Day \n"))
+        y = int(input("Enter Year (YYYY) \n"))
 
-def on_calc():
+        d1 = date(y, m, d)
+        d1 = d1.strftime("%m/%d/%Y")
 
-    #Access to global variables
-    global globalBank 
-    global globalPreview
-    
+    else:
+        d1 = today.strftime("%m/%d/%Y")
+
+    return d1
+
+
+def on_calc(cearned, ctaken, reason):
+    global gPreview  # Access to global variable
+    global gBank
+
+    # sets up variables and gets Earned or Taken Values
+    bank = gBank
+    dte = get_Date()
+
     print("\n")
     print("Calculating New Daily Bal...")
 
-    #sets up variables and gets Earned or Taken Values
-    #date = self.lblCurDate.get_text()
-    #calcearned = self.entryEarned.get_text()
-    #calctaken = self.entryTaken.get_text()
-    #preview = self.combobox1.get_active_text()
-    #taken = self.entryTaken.get_text()
-    #bank = globalBank
-    
-    #If Earned is blank, it will be set to zero, otherwise it gets Earned
-    #entry
-    if calcearned == "":
-        calcearned = 0.0
-    else:
-        calcearned = self.entryEarned.get_text()
-
-    #if Taken is blank, it will be set to zero, otherwise it gets Taken
-    #entry
-    if calctaken == "":
-        calctaken = 0.0
-    else:
-        calctaken = self.entryTaken.get_text() 
-
-    #convert string variables to decimal(float) for calculation
-    calcearned = float(calcearned) * 1.5
-    calctaken = float(calctaken)
-    newbal = calcearned - calctaken
+    # convert string variables to decimal(float) for calculation
+    cearned = float(cearned) * 1.5
+    ctaken = float(ctaken)
+    newbal = cearned - ctaken
     newbank = newbal + float(bank)
+    gBank = str(newbank)
 
-    #convert to back to string to display in label
+    # convert to back to string to display in label
     newbal = str(newbal)
     newbank = str(newbank)
     print("\n")
     print("Setting Preview of New Balance Applied...")
+    print()
 
-    #shows current calculation daily balance
-    #self.lbl6.set_text(newbal)
-    
-    #shows current preview of time entry prior to writing text file
-    print("Total time to enter on affidavit = " 
-    + newbal + " hrs\n" + "-"*145 + "\n" + "Date" + " "*25 + "Reason"
-    + " "*25 + "Earned" + " "*25 + "Taken" + " "*25 + "New Balance\n"
-    + "-"*9 + " "*24 + "-"*12 + " "*25 + "-"*12 + " "*25 + "-"*10
-    + " "*25 + "-"*21 + "\n" + str(date) + " "*13 + str(preview)
-    + " "*25 + str(calcearned) + " "*40 + str(taken) + " "*30
-    + newbank)
+    # shows current preview of time entry prior to writing text file
+    print("Total time to enter on affidavit = " + newbal + " hrs\n"
+          + "-" * 115 + "\n"
+          + "Date" + " " * 18 + "Reason" + " " * 16 + "Earned" + " " * 18 + "Taken" + " " * 17 + "New Balance\n"
+          + "-" * 10 + " " * 12 + "-" * 10 + " " * 12 + "-" * 12 + " " * 12 + "-" * 10
+          + " " * 12 + "-" * 15 + "\n" + str(dte) + " " * 12 + reason + " " * 12
+          + str(cearned) + " " * 21 + str(ctaken) + " " * 19
+          + newbank)
+    print()
 
-    #What gets put into run file on applying calc
-    globalPreview = (str(date) + " "*8 + str(preview) 
-    + " "*10 + str(calcearned) + " "*18 + str(taken) + " "*17 
-    + newbank + "\n"
-    + "-"*90 + "\n")
+    # What gets put into run file on applying calc
+    gPreview = (str(dte) + " " * 7 + reason + " " * 6 + str(cearned)
+                + " " * 19 + str(ctaken) + " " * 17
+                + newbank + "\n"
+                + "-" * 90 + "\n")
+
 
 def on_apply():
-    global globalBank #access global variable
+    global gBank  # access global variable
+    global gPreview
     print("\n")
     print("Applying New Daily Balance to Bank...")
-    
-    #incorporate to opening file
-    bank2 = globalBank
-    newDaybal= self.lbl6.get_text() #get daily balance text
+    print()
+
+    # incorporate to opening file
+    bank2 = float(gBank)
+    newDaybal = float(gDaily_Bank)  # get daily balance text
     newbank2 = float(newDaybal) + bank2
-    
-    globalBank = (newbank2) #update global variable with new balance
-    newbank2 = str(newbank2) #convert to string to put into label and file
-    #self.lbl5.set_text(newbank2) #put in bank label
-    
-    #writes to the bankfile
-    f = open("/home/sgarrison/temp/test1.txt", "w")
+
+    gBank = (str(newbank2))  # update global variable with new balance
+    newbank2 = str(newbank2)  # convert to string to put into label and file
+
+    # writes to the bank file
+    f = open("D:\Temp/test1.txt", "w")
     f.write(newbank2)
     f.close()
-    
-    #writes to runfile
-    f2 = open("/home/sgarrison/temp/test2.txt", "a")
-    f2.write(globalPreview)
-    f2.close()
-    
 
-#start program
+    # writes to runfile
+    f2 = open("D:\Temp/test2.txt", "a")
+    f2.write(gPreview)
+    f2.close()
+
+
+# start program
 print("Comptime Calculator")
 print("---------------------------------------------------------------")
 
-
-#checks to see if the bankfile exists.  If it does, it pulls from it.
-if os.path.isdir("/home/sgarrison/temp/") and os.path.isfile("/home/sgarrison/temp/test1.txt"):
-    f = open("/home/sgarrison/temp/test1.txt", "r")
+# checks to see if the bank file exists.  If it does, it pulls from it.
+if os.path.isdir("D:\Temp/") and os.path.isfile("D:\Temp/test1.txt"):
+    f = open("D:\Temp/test1.txt", "r")
     text = f.readline()
-    globalBank = float(text)        
+    gBank = float(text)
     f.close()
-    
+
 else:
-    #if bankfile dosen't exist, it creates it with a 0.0 balance then
-    #reads from it.
+    # if bank file doesn't exist, it creates it with a 0.0 balance then
+    # reads from it.
     startBal = "0.0"
-    f = open("/home/sgarrison/temp/test1.txt", "w")
+    f = open("D:\Temp/test1.txt", "w")
     f.write(startBal)
     f.close()
-    
-    f = open("/home/sgarrison/temp/test1.txt", "r")
-    text = f.readline()
-    globalBank = float(text)        
-    f.close()
-    
-    f = open("/home/sgarrison/temp/test2.txt", "w")
-    f.write("Orange County Juvenile Probation Dept.\n"
-    + "-"*40 + "\n" 
-    + "Personal Comptime Sheet for: Shon Garrison\n"
-    + "\n" 
-    + "Date" + " "*13 + "Reason" + " "*11 + "Earned" + " "*16 
-    + "Taken" + " "*15 + "New Balance\n"
-    + "-"*9 + " "*7 + "-"*12 + " "*6 + "-"*7 + " "*15 
-    + "-"*6  + " "*14 + "-"*12 + "\n")
 
+    f = open("D:\Temp/test1.txt", "r")
+    text = f.readline()
+    gBank = float(text)
+    f.close()
+
+    # Creates running file skeleton
+    f = open("D:\Temp/test2.txt", "w")
+    f.write("Orange County Juvenile Probation Dept.\n"
+            + "-" * 40 + "\n"
+            + "Personal Comptime Sheet for: Shon Garrison\n"
+            + "\n"
+            + "Date" + " " * 13 + "Reason" + " " * 11 + "Earned" + " " * 16 + "Taken" + " " * 15 + "New Balance\n"
+            + "-" * 9 + " " * 7 + "-" * 12 + " " * 6 + "-" * 7 + " " * 15
+            + "-" * 6 + " " * 14 + "-" * 12 + "\n")
+    f.close()
 
 while running:
-    print("Your Current Balance is " + str(globalBank))
-
+    print()
+    print("Your Current Balance is " + str(gBank))
     print("---------------------------------------------------------------")
+    print()
 
     print("What do you want to do?\n")
     print("1.) Add a transaction")
@@ -164,21 +161,25 @@ while running:
 
         if transaction == 1:
             print()
-            print("Examples include--On-Call, Det Visit, Special Grp, Tranpsort, Program, or you may write your own.")
-            print("Reason for earned time?\n")
-            reason = str(input())
-            quit()
+            print("Examples include -- On-Call, Det Visit, Special Grp, Transport, Program, or you may write your own.")
+            reason = input("Reason for earned time?\n")
+            reason = reason.ljust(11, " ")
+            earned = str(input("Enter time in \"0.00\" format\n"))
+            taken = float(0.00)
+            on_calc(earned, taken, reason)
+            on_apply()
 
         else:
             print()
-            print("Examples include--Sick, Personal, or you may write your own.")
-            print("Reason for time taken?\n")
-            reason = str(input())
-            quit()
+            print("Examples include -- Sick, Personal, or you may write your own.")
+            reason = str(input("Reason for time taken?\n"))
+            reason = reason.ljust(11, " ")
+            taken = str(input("Enter time in \"0.00\" format\n"))
+            earned = float(0.00)
+            on_calc(earned, taken, reason)
+            on_apply()
 
     else:
 
         quit()
         running = False
-
-
